194722b28ae3efe0443ebd5f566baa498e362f60
Your Name <you@example.com>
2025-02-07 23:46:31 +0000

    position intro logo and fadeout script

---
+++ Assets/JS/script.js
@@ -0,0 +1,14 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+    if (!sessionStorage.getItem("introShown")) {
+      setTimeout(function () {
+        document.getElementById("intro-overlay").style.opacity = "0";
+        setTimeout(function () {
+          document.getElementById("intro-overlay").style.display = "none";
+        }, 1000);
+      }, 3000);
+      sessionStorage.setItem("introShown", "true");
+    } else {
+      document.getElementById("intro-overlay").style.display = "none";
+    }
+  });
